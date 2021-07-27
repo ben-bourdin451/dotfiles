21f98b5a1e6b361272e60eda483a66902cf9febc
Ben Bourdin <ben.bourdin451@gmail.com>
2021-07-27 11:01:48 +0100

    aws mfa update

--- aws_mfa.sh
+++ aws_mfa.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-read -p 'MFA Token: ' mfavar
-line=$(aws sts get-session-token --profile production --serial-number $AWS_MFA_SERIAL --token-code $mfavar --duration-seconds 129600 --output text)
+read -p 'MFA Token: ' otp
+
+line=$(aws sts get-session-token --profile user --serial-number $AWS_MFA_SERIAL --token-code $otp --duration-seconds 129600 --output text)
 echo $line | awk 'BEGIN {FS=" "}; {printf("[default]\naws_access_key_id=%s\naws_secret_access_key=%s\naws_session_token=%s\n#expiry %s",$2,$4,$5,$3)}' > ~/.aws/credentials
